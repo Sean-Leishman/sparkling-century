8b7dd809a505e5acae43c55ea57c36078be55ecb
Sean Leishman <leishmansean@gmail.com>
2024-01-08 13:27:51 +0300

    Update API query used and format of query

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,19 +3,22 @@ import { SWOP_API_KEY } from "$env/static/private"
 export const ssr = false;
 
 export const load: PageServerLoad = async () => {
-    let satellites = []; 
 
-    const satellites = await fetch('https://api.n2yo.com/rest/v1/satellite/tle/25544', {
+    let lat = 41.702;
+    let lng = -76.014;
+    let alt = 0;
+    let radius = 90;
+    let category_id = 0;
+
+    const response = await fetch(`https://api.n2yo.com/rest/v1/satellite/above/${lat}/${lng}/${alt}/${radius}/${category_id}&apiKey=${SWOP_API_KEY}`, {
         method:'GET',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `ApiKey ${SWOP_API_KEY}`
         },
     });
 
-    const item = await satellites.json()
+    const satellites = await response.json()
+    console.log(satellites,`https://api.n2yo.com/rest/v1/satellite/above/${lat}/${lng}/${alt}/${radius}/${category_id}` )
 
-    return {
-        satellites: item
-    }
+    return satellites
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,17 @@
     import { createScene } from "$lib/scene";
     import { onMount } from "svelte";
 
+    export let data: PageData;
     let el;
 
     onMount(() => {
-        createScene(el)
+        createScene(el, data)
     });
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<p>{data}</p>
 <canvas bind:this={el}></canvas>
